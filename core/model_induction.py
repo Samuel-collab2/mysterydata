5935278bb762aee0c8af92cfe0ab7852f7bec34e
Brandon Semilla <semibran@gmail.com>
2022-10-29 15:19:05 -0700

    first pass on recursive induction

--- core/model_induction.py
+++ core/model_induction.py
@@ -4,28 +4,32 @@ import pandas as pd
 
 class BinaryDecisionTree:
 
-    def __init__(self):
-        self._tree = None
-        self._dataset_label_name = None
-
-    def _find_root_entropy(self, label):
+    @staticmethod
+    def _find_root_entropy(label):
         num_labels = len(label)
         num_true = sum(label)
         num_false = num_labels - num_true
+        if num_true == 0 or num_false == 0:
+            return 0
 
         p_true = num_true / num_labels
         p_false = num_false / num_labels
-
         return -(p_true * log2(p_true)
             + p_false * log2(p_false))
 
+    @staticmethod
+    def _find_feature_values(samples, feature):
+        return list(set(samples.loc[:, feature]))
+
+
+    def __init__(self):
+        self._tree = None
+        self._dataset_num_features = 0
+        self._dataset_label_name = None
+
     def _find_entropy(self, samples, feature):
-        feature_values = list(set(samples.loc[:, feature]))
-
         entropy = 0
-        for feature_value in feature_values:
-            print(f'\n-- {feature}={feature_value}')
-
+        for feature_value in self._find_feature_values(samples, feature):
             num_samples = len(samples)
             feature_value_samples = samples[samples[feature] == feature_value]
             num_feature_value_samples = len(feature_value_samples)
@@ -41,15 +45,11 @@ class BinaryDecisionTree:
 
             p_true = num_true_samples / num_feature_value_samples
             p_false = num_false_samples / num_feature_value_samples
-            print(f'p_true={num_true_samples}/{num_feature_value_samples}')
-            print(f'p_false={num_false_samples}/{num_feature_value_samples}')
 
             feature_entropy = -(p_true * log2(p_true)
                 + p_false * log2(p_false))
             feature_weight = num_feature_value_samples / num_samples
 
-            print(f'entropy={feature_entropy:.4f}'
-                f'\nweight={num_feature_value_samples}/{num_samples}')
             entropy += feature_weight * feature_entropy
 
         return entropy
@@ -57,24 +57,47 @@ class BinaryDecisionTree:
     def _find_information_gain(self, samples, feature, entropy):
         return entropy - self._find_entropy(samples, feature)
 
-    def train(self, train_features, train_label):
-        self._dataset_label_name = train_label.name
+    def _induce(self, samples):
+        ply = self._dataset_num_features - len(samples.columns) + 2
 
-        root_entropy = self._find_root_entropy(train_label)
-        print(f'Root entropy: {root_entropy:.4f}')
+        if len(samples.columns) == 1:
+            return  # STOPPAGE CRITERIA: label is only column in dataset
 
-        train_samples = pd.concat((train_features, train_label), axis=1)
+        root_entropy = self._find_root_entropy(samples.loc[:, self._dataset_label_name])
+        if root_entropy == 0:
+            # STOPPAGE CRITERIA: perfect classification: all samples in dataset have the same label value
+            return samples.loc[:, self._dataset_label_name].any()
+
         feature_gains = {feature: self._find_information_gain(
-            samples=train_samples,
+            samples=samples,
             feature=feature,
             entropy=root_entropy,
-        ) for feature in train_features.columns}
+        ) for feature in samples.columns
+            if feature != self._dataset_label_name}
 
-        for feature, feature_gain in feature_gains.items():
-            print(f'{feature} information gain = {feature_gain:.4f}')
+        feature_gain_values = list(feature_gains.values())
+        max_feature_gain = max(feature_gain_values)
+        max_feature_index = feature_gain_values.index(max_feature_gain)
+        max_feature = list(feature_gains.keys())[max_feature_index]
+
+        other_features = [feature for feature in samples.columns if feature != max_feature]
+        for feature_value in self._find_feature_values(samples, max_feature):
+            child_samples = samples[samples[max_feature] == feature_value].loc[:, other_features]
+            print(f'Induce {max_feature}'
+                f' through {feature_value}'
+                f' at ply {ply + 1}'
+                f' ({len(child_samples)} samples)')
+            self._induce(child_samples)
+
+    def train(self, train_features, train_label):
+        self._dataset_num_features = len(train_features.columns)
+        self._dataset_label_name = train_label.name
+        train_samples = pd.concat((train_features, train_label), axis=1)
+        self._induce(train_samples)
 
     def predict(self, test_features):
-        return [False for i, x in test_features.iterrows()]
+        return [False
+            for i, x in test_features.iterrows()]
 
 
 def train_decision_tree(train_features, train_label):
