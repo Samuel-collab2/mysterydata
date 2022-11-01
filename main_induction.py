52887f453ba53cd102dd1b117b31fb7ff344bfeb
Brandon Semilla <semibran@gmail.com>
2022-10-31 22:28:34 -0700

    setup decision tree classifier tests

--- main_induction.py
+++ main_induction.py
@@ -1,44 +1,60 @@
-from os.path import join
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
-from core.preprocessing import is_categorical_column, separate_features_label, split_training_test
-from core.model_induction import NullDecisionTreeInduction, BinaryDecisionTreeInduction, train_decision_tree
-from core.constants import DATASET_LABEL_NAME, DATASET_TRAIN_RATIO, OUTPUT_DIR
+from core.preprocessing import separate_features_label, split_training_test, \
+    expand_dataset
+from core.model_induction import NullDecisionTreeInduction
+from core.constants import DATASET_LABEL_NAME, DATASET_TRAIN_RATIO
 
+
+def is_categorical_column(column):
+    return column.dtype == "uint8"
 
 def _extract_categorical_dataset(dataset):
     categorical_feature_names = [column_name for column_name in dataset.columns
         if is_categorical_column(dataset[column_name])]
-    return dataset.loc[:,categorical_feature_names]
+    return dataset.loc[:, categorical_feature_names]
 
 
 def perform_decision_tree_induction(dataset):
     features, labels = separate_features_label(dataset, DATASET_LABEL_NAME)
-    categorical_features = _extract_categorical_dataset(features)
-    categorical_labels = pd.Series(labels).map(bool)
+    features_expanded = expand_dataset(features)
+    labels_binary = pd.Series(labels).map(bool)
 
     (train_features, train_labels), (test_features, test_labels) = split_training_test(
-        categorical_features,
-        categorical_labels,
+        features_expanded,
+        labels_binary,
         train_factor=DATASET_TRAIN_RATIO,
         shuffle=True,
     )
 
-    export_path = join(OUTPUT_DIR, 'decision_tree.json')
-    try:
-        model = BinaryDecisionTreeInduction.load_from_path(export_path)
-        print(f'Loaded decision tree dump from {export_path}')
-    except FileNotFoundError:
-        print('Training induction model based on categorical features...')
-        model = train_decision_tree(train_features, train_labels)
-        model.dump(export_path)
-        print(f'Wrote decision tree dump to {export_path}')
-
-    print("\nEvaluating performance of null induction model...")
+    print('\nEvaluating performance of null induction model...')
     evaluate_model(NullDecisionTreeInduction(), test_features, test_labels)
 
-    print("\nEvaluating performance of categorical feature-based induction model...")
+    print('\nEvaluating performance of base DecisionTreeClassifier...')
+    model = DecisionTreeClassifier()
+    model.fit(train_features, train_labels)
     evaluate_model(model, test_features, test_labels)
+
+    print('\nEvaluating performance of entropy-based DecisionTreeClassifier...')
+    model = DecisionTreeClassifier(criterion='entropy')
+    model.fit(train_features, train_labels)
+    evaluate_model(model, test_features, test_labels)
+
+    train_features = _extract_categorical_dataset(train_features)
+    test_features = _extract_categorical_dataset(test_features)
+
+    print('\nEvaluating performance of categorical DecisionTreeClassifier...')
+    model = DecisionTreeClassifier()
+    model.fit(train_features, train_labels)
+    evaluate_model(model, test_features, test_labels)
+
+    print('\nEvaluating performance of categorical entropy-based DecisionTreeClassifier...')
+    model = DecisionTreeClassifier(criterion='entropy')
+    model.fit(train_features, train_labels)
+    evaluate_model(model, test_features, test_labels)
+
 
 def evaluate_model(model, test_features, test_labels):
     pred_labels = model.predict(test_features)
@@ -47,8 +63,8 @@ def evaluate_model(model, test_features, test_labels):
     num_rows = len(test_features)
     num_predicted_accepts = sum(pred_labels)
     num_observed_accepts = sum(test_labels)
-    accuracy = sum([test_labels[i] == y_hat
-        for i, y_hat in enumerate(pred_labels)]) / num_rows
+    accuracy = accuracy_score(test_labels, pred_labels)
+
     print(f'Prediction: {num_predicted_accepts}/{num_rows} claims accepted'
         f'\nActual:     {num_observed_accepts}/{num_rows} claims accepted'
         f'\nDecision tree induction model achieved {accuracy * 100:.2f}% accuracy.')
