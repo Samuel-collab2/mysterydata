fc52a8370283cfd0d70dd7917047306989932397
Brandon Semilla <semibran@gmail.com>
2022-11-16 18:41:00 -0800

    first pass on iterative induction setup

--- main_induction_iterative.py
+++ main_induction_iterative.py
@@ -11,6 +11,9 @@ from core.constants import DATASET_LABEL_NAME, DATASET_TRAIN_RATIO
 from core.constants_feature_set import SIGNIFICANT_AUGMENTED_COLUMNS
 
 
+NUM_EPOCHS = 10
+
+
 def get_induction_data(train_data, test_data):
     train_features, train_labels = separate_features_label(train_data, DATASET_LABEL_NAME)
     test_features = test_data
@@ -53,24 +56,45 @@ def get_induction_data(train_data, test_data):
     )
 
 
-def sandbox_iterative_induction(train_data, test_data):
-    train_features, train_labels, test_features = get_induction_data(train_data, test_data)
+def run_simulation(train_features, train_labels, model=None, epoch=0):
     x_train, x_test, y_train, y_test = train_test_split(
-        train_features[SIGNIFICANT_AUGMENTED_COLUMNS],
+        train_features,
         train_labels,
         train_size=DATASET_TRAIN_RATIO,
-        shuffle=True,
-        random_state=0,
     )
     x_train, y_train = balance_binary_dataset(x_train, y_train, skew_false=6)
 
-    model = RandomForestClassifier(n_estimators=50)
-    model.fit(x_train, y_train)
-    pred_labels = model.predict(x_test)
+    if model is None:
+        model = RandomForestClassifier(n_estimators=50)
+        model.fit(x_train, y_train)
+
+    evaluate_model(model, x_train, x_test, y_train, y_test, epoch=epoch)
+    return model
+
+def evaluate_model(model, x_train, x_test, y_train, y_test, epoch):
+    print(f'\n-- Epoch {epoch}/{NUM_EPOCHS}')
     print(f'Training accuracy:\t{model.score(x_train, y_train)*100:.4f}%')
     print(f'Validation accuracy:\t{model.score(x_test, y_test)*100:.4f}%')
     print(f'Training F1:\t\t{f1_score(y_train, model.predict(x_train))*100:.4f}%')
-    print(f'Validation F1:\t\t{f1_score(y_test, pred_labels)*100:.4f}%')
+    print(f'Validation F1:\t\t{f1_score(y_test, model.predict(x_test))*100:.4f}%')
+
+def sandbox_iterative_induction(train_data, test_data):
+    train_features, train_labels, test_features = get_induction_data(train_data, test_data)
+    train_features = train_features[SIGNIFICANT_AUGMENTED_COLUMNS]
+    test_features = test_features[SIGNIFICANT_AUGMENTED_COLUMNS]
+
+    model = run_simulation(train_features, train_labels, epoch=1)
+
+    for epoch in range(2, NUM_EPOCHS):
+        pred_labels = model.predict(test_features)
+        pred_labels = pd.Series(pred_labels, name=DATASET_LABEL_NAME)
+        model = run_simulation(
+            pd.concat((train_features, test_features)),
+            pd.concat((train_labels, pred_labels)),
+            epoch=epoch
+        )
+
+    run_simulation(train_features, train_labels, model, epoch=NUM_EPOCHS)
 
 
 if __name__ == '__main__':
