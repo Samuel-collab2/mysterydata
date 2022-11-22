d16465ef6c0d1eab11ae08af8d9fc5c07ed1d337
Brandon Semilla <semibran@gmail.com>
2022-11-21 18:59:33 -0800

    Add model exporter (#20)

--- core/model_set.py
+++ core/model_set.py
@@ -96,6 +96,49 @@ def get_model_set_data(train_dataset, test_features):
         (accept_train_features, accept_train_label, processed_test_features, processed_train_features), \
         raw_train_label
 
+def get_model_set_train_data(train_dataset):
+    """
+    Preprocesses training data.
+    :param: a data frame containing the training data
+    :return: (
+        (induction_features, induction_labels, _, _),
+        (regression_features, regression_labels, _, _),
+    )
+    """
+    train_features, train_labels = separate_features_label(train_dataset, DATASET_LABEL_NAME)
+
+    categorical_columns = get_categorical_columns(train_dataset)
+    categorical_train_features = train_features.loc[:, categorical_columns]
+    expanded_train_features = expand_dataset_deterministic(
+        train_features,
+        load_determining_dataset(),
+        categorical_columns
+    )
+    augmented_train_features = create_augmented_features(
+        train_features,
+        SIGNIFICANT_AUGMENTED_COLUMNS
+    )
+
+    induction_train_features = pd.concat((
+        expanded_train_features,
+        augmented_train_features,
+        categorical_train_features,
+    ), axis='columns')
+    induction_train_columns = induction_train_features.columns
+
+    induction_train_labels = convert_label_boolean(train_labels)
+    induction_data = (induction_train_features, induction_train_labels,
+        pd.DataFrame(columns=induction_train_columns),
+        pd.DataFrame(columns=induction_train_columns))
+
+    accept_data, _ = split_claims_accept_reject(induction_train_features, train_labels)
+    accept_train_features, accept_train_label = accept_data
+    regression_data = (accept_train_features, accept_train_label,
+        pd.DataFrame(columns=induction_train_columns),
+        pd.DataFrame(columns=induction_train_columns))
+
+    return induction_data, regression_data
+
 def enumerate_model_set_predictions(train_dataset, test_dataset, model_sets):
     """
     Output predictions and training MAE for the given model sets.
@@ -119,39 +162,63 @@ def enumerate_model_set_predictions(train_dataset, test_dataset, model_sets):
     submission_data = get_model_set_data(train_dataset, test_dataset)
 
     for index, model_set in enumerate(model_sets):
-        print(f'Predicting: {model_set.name}...')
-
-        induction_data, regression_data, evaluation_label = submission_data
-        for induction_modifier in model_set.induction_modifiers:
-            induction_data = induction_modifier(*induction_data)
-
-        for regression_modifier in model_set.regression_modifiers:
-            regression_data = regression_modifier(*regression_data)
-
-        induction_train_features, induction_train_label, \
-            induction_test_features, induction_evaluation_features = induction_data
-
-        regression_train_features, regression_train_label, \
-            regression_test_features, regression_evaluation_features = regression_data
-
-        model = train_composite(
-            induction_train_features,
-            induction_train_label,
-            regression_train_features,
-            regression_train_label,
-            model_set.train_induction_model,
-            model_set.train_regression_model,
-            proba_threshold=model_set.proba_threshold,
-        )
-
-        evaluation_predictions = model.predict(
-            induction_evaluation_features,
-            regression_evaluation_features
-        )
-
-        test_predictions = model.predict(
-            induction_test_features,
-            regression_test_features,
-        )
-
+        evaluation_predictions, evaluation_label, test_predictions = get_model_set_predictions(model_set, submission_data)
         yield index, evaluation_predictions, evaluation_label, test_predictions
+
+def modify_model_set_train_data(model_set, induction_data, regression_data):
+    for induction_modifier in model_set.induction_modifiers:
+        induction_data = induction_modifier(*induction_data)
+
+    for regression_modifier in model_set.regression_modifiers:
+        regression_data = regression_modifier(*regression_data)
+
+    return induction_data, regression_data
+
+def train_model_from_model_set(model_set, induction_data, regression_data):
+    induction_data, regression_data = modify_model_set_train_data(
+        model_set,
+        induction_data,
+        regression_data,
+    )
+
+    induction_train_features, induction_train_label, _, _ = induction_data
+    regression_train_features, regression_train_label, _, _ = regression_data
+
+    model = train_composite(
+        induction_train_features,
+        induction_train_label,
+        regression_train_features,
+        regression_train_label,
+        model_set.train_induction_model,
+        model_set.train_regression_model,
+        proba_threshold=model_set.proba_threshold,
+    )
+
+    return model
+
+def get_model_set_predictions(model_set, submission_data):
+    print(f'Predicting: {model_set.name}...')
+
+    induction_data, regression_data, evaluation_label = submission_data
+    model = train_model_from_model_set(model_set, induction_data, regression_data)
+
+    induction_data, regression_data = modify_model_set_train_data(
+        model_set,
+        induction_data,
+        regression_data,
+    )
+
+    _, _, induction_test_features, induction_evaluation_features = induction_data
+    _, _, regression_test_features, regression_evaluation_features = regression_data
+
+    evaluation_predictions = model.predict(
+        induction_evaluation_features,
+        regression_evaluation_features
+    )
+
+    test_predictions = model.predict(
+        induction_test_features,
+        regression_test_features,
+    )
+
+    return evaluation_predictions, evaluation_label, test_predictions
